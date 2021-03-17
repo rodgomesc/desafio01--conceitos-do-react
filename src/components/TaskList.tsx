4b3c0ddf215d4242bcec40a2d8d4e1a4a503aef9
Rodrigo <rodgomesc@gmail.com>
2021-03-16 23:35:21 -0300

    chore: finished first challenge

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+
 
 import '../styles/tasklist.scss'
 
@@ -13,24 +14,52 @@ interface Task {
 export function TaskList() {
   const [tasks, setTasks] = useState<Task[]>([]);
   const [newTaskTitle, setNewTaskTitle] = useState('');
+  const [taskError, setTaskError] = useState('')
+
+
+  useEffect(() => {
+    if (!newTaskTitle.length) {
+      return setTaskError("task title can't be empty!")
+    } else {
+      setTaskError('')
+    }
+  },[newTaskTitle])
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    !taskError.length && setTasks([
+      ...tasks,
+       {
+        id: Number(Number(Math.random() * 1e11).toFixed(0)),
+        title: newTaskTitle,
+        isComplete: false
+       }
+    ]);
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const changedTasks = tasks.map(task => {
+      return task.id === id ?  { ...task, isComplete: !task.isComplete } : task
+    })
+
+    setTasks(changedTasks)
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const changedTasks = tasks.filter(task => task.id !== id);
+    setTasks(changedTasks)
+    
   }
 
   return (
     <section className="task-list container">
       <header>
         <h2>Minhas tasks</h2>
-
+        {!!taskError.length && (
+           <strong style={{color: 'red'}}>{taskError}</strong>
+          )}
         <div className="input-group">
           <input 
             type="text" 
@@ -40,7 +69,7 @@ export function TaskList() {
           />
           <button type="submit" data-testid="add-task-button" onClick={handleCreateNewTask}>
             <FiCheckSquare size={16} color="#fff"/>
-          </button>
+          </button>         
         </div>
       </header>
 
